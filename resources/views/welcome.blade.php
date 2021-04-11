72539a9eaa1b428482f93965e763eb700056cd57
Gilles Pinchart <gilles.pinchart@scabel.eu>
2021-04-11 21:08:58 +0200

    modif lama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,11 +17,6 @@
         </style>
 
         <style>
-{{--            {{ dd(public_path('fonts\The_Juke_Box-FFP.ttf'))  }}--}}
-            @font-face {
-                font-family: jukeboxxx;
-                src: url('./public/fonts/gabriele-br.TTF');
-            }
             body header h1{
                 color: black;
                 font-size: xxx-large;
@@ -46,10 +41,18 @@
     <section>
         <div id="content-ambiance">
 {{--            {{ dd(public_path('files\mountainsound1.mp3')) }}--}}
-            <audio id="player" autoplay controls><source src="{{public_path('files\mountainsound1.mp3')}}" type="audio/mp3"></audio>
+            <audio id="ID" autoplay loop>
+                <source src="files/mountainsound1.mp3" type="audio/mp3" />
+            </audio>
+{{--            <audio id="player" autoplay><source src="files/mountainsound1.mp3" type="audio/mp3"></audio>--}}
         </div>
         <div id="content-lama" class="text-center">
-            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="50">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="100">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="200">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="300">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="400">
+            <img id="lama" src="https://i.pinimg.com/originals/5a/3c/bf/5a3cbf162b6a195d74be5e180ae98a39.gif" alt="lama" width="500">
         </div>
     </section>
 
@@ -57,15 +60,20 @@
 
     </footer>
 
-    @section('script')
+{{--    @section('script')--}}
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
             <script>
-                window.onload = function() {
-                    var context = new AudioContext();
+                document.addEventListener('pointermove', musicPlay);
+                function musicPlay() {
+                    document.getElementById('ID').play();
+                    document.removeEventListener('click', musicPlay);
                 }
+                $('body').on('click', function(e){
+                    console.log('testsetsete');
+                })
             </script>
-    @endsection
+{{--    @endsection--}}
     </body>
     </html>
